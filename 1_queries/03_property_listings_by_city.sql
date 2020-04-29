16f2a3cf1c2ad1fbf19be131f1f55b978268ab71
Rance Campbell <rancewcampbell@gmail.com>
2020-04-29 21:34:45 +0000

    query for property by city

---
+++ 1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,9 @@
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE 'Vancouver'
+GROUP BY properties.id
+HAVING AVG(rating) >=4
+ORDER BY cost_per_night
+LIMIT 10;
+
